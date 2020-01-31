3a26f150ec335ea8f3c44157bc73c90d5a106ed2
Zakir Hussain <zack.mesacc@gmail.com>
2020-01-31 17:22:53 +0500

    signup users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :articles
   root 'pages#home'
+  get 'signup', to: 'users#new'
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+  resources :users, except: [:new]
 end
