f18f92c3f6573a07e5881c59f8f9aa9f744c25d2
Zakir Hussain <zack.mesacc@gmail.com>
2020-02-04 11:17:17 +0500

    add pagination to view

--- Gemfile
+++ Gemfile
@@ -15,6 +15,8 @@ gem 'coffee-rails', '~> 4.2'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'jquery-rails'
+gem 'will_paginate', '~> 3.1.0'
+gem 'bootstrap-will_paginate', '~> 0.0.10'
 
 
 group :development, :test do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
 class UsersController < ApplicationController
 
     def index
-        @users = User.all
+        @users = User.paginate(page: params[:page], per_page: 3)
     end
 
     def show
         @user = User.find(params[:id])
+        @user_articles = @user.articles.paginate(page: params[:page], per_page: 3)
     end
 
     def new 
